b8da5ec7980ebc3c9d36d65e6bfd86e6db8e8f39
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-03-28 15:39:04 +0700

    init: first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/KilimanjaroApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.kilimanjaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilimanjaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KilimanjaroApplication.class, args);
+    }
+
+}
