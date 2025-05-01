15401ad12380230acaa8e6787b6e9b7f50fa6f8f
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-01 13:58:18 +0700

    feat: add base user repository.

---
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/repository/BaseUserRepository.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.kilimanjaro.repository;
+
+import id.ac.ui.cs.advprog.kilimanjaro.model.BaseUser;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BaseUserRepository extends JpaRepository<BaseUser, Long> {
+    Optional<BaseUser> findByFullName(@NotBlank @Size(max = 100) String fullName);
+
+    Optional<BaseUser> findByEmail(@NotBlank @Email @Size(max = 100) String email);
+
+    Boolean existsByFullName(@NotBlank @Size(max = 100) String fullName);
+
+    Boolean existsByEmail(@NotBlank @Email @Size(max = 100) String email);
+}
