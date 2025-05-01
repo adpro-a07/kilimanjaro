98da1b5507976722d9989a462e2ffad765078255
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-01 19:13:38 +0700

    refactor: remove unused import.

--- src/main/java/id/ac/ui/cs/advprog/kilimanjaro/dto/RegisterTechnicianRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/dto/RegisterTechnicianRequest.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.kilimanjaro.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
