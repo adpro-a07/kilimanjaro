e49ff105b82a3d070f2bbba5982a9a8b4d8d8e41
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-01 20:21:58 +0700

    refactor[GREEN]: remove unused imports.

--- src/main/java/id/ac/ui/cs/advprog/kilimanjaro/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/controller/exception/GlobalExceptionHandler.java
@@ -2,11 +2,8 @@ package id.ac.ui.cs.advprog.kilimanjaro.controller.exception;
 
 import id.ac.ui.cs.advprog.kilimanjaro.authentication.exceptions.InvalidCredentialsException;
 import id.ac.ui.cs.advprog.kilimanjaro.authentication.exceptions.UserAlreadyExistsException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
