508a95dd755faaa6f43337245324a5850a4ae123
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-01 19:50:34 +0700

    chore: remove unused import.

--- src/main/java/id/ac/ui/cs/advprog/kilimanjaro/model/Customer.java
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/model/Customer.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.kilimanjaro.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
