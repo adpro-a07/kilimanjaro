8625b8fcda792a011e2a210bbcf089d4e596afc9
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-04 20:40:55 +0700

    refactor: modify GenericResponse to return a prettier format.

--- src/main/java/id/ac/ui/cs/advprog/kilimanjaro/dto/GenericResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/dto/GenericResponse.java
@@ -3,39 +3,26 @@ package id.ac.ui.cs.advprog.kilimanjaro.dto;
 import lombok.Getter;
 
 import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class GenericResponse<T> {
-    private final BaseResponse meta;
+    private final boolean success;
+    private final String message;
+    private final String timestamp;
     private final T data;
 
     public GenericResponse(boolean success, String message, T data) {
-        this.meta = new BaseResponse(success, message);
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return meta.getMessage();
-    }
-
-    public boolean isSuccess() {
-        return meta.isSuccess();
-    }
-
-    public Instant getTimestamp() {
-        return meta.getTimestamp();
-    }
-}
-
-@Getter
-class BaseResponse {
-    private final boolean success;
-    private final String message;
-    private final Instant timestamp;
-
-    public BaseResponse(boolean success, String message) {
         this.success = success;
         this.message = message;
-        this.timestamp = Instant.now();
+        this.data = data;
+        this.timestamp = formatTimestamp(Instant.now());
+    }
+
+    private String formatTimestamp(Instant timestamp) {
+        return DateTimeFormatter.ISO_ZONED_DATE_TIME
+                .withZone(ZoneOffset.UTC)
+                .format(timestamp);
     }
 }
