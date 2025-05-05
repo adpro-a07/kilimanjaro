914f1452d0fb903491ca0d6c1b17bfaf5fc119ce
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-06 01:35:03 +0700

    feat: add custom exceptions for grpc integration.

---
+++ src/main/java/id/ac/ui/cs/advprog/kilimanjaro/authentication/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.kilimanjaro.authentication.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
