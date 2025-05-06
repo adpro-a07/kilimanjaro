431b6b762d40bdb1b8174bee49fb2bae95e10270
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-06 12:04:27 +0700

    refactor: rename AuthService test class.

--- src/test/java/id/ac/ui/cs/advprog/kilimanjaro/service/AuthServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/kilimanjaro/service/AuthServiceImplTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class AuthServiceTest {
+public class AuthServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
